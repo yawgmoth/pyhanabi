556170a232c2719853e1e4b8e1867fad8d9c71c2
Markus Eger <markus.eger@gmail.com>
2019-08-24 14:58:42 -0600

    add timed player

--- hanabi.py
+++ hanabi.py
@@ -484,7 +484,85 @@ class SelfRecognitionPlayer(Player):
             self.last_trash = game.trash[:]
             self.played = game.played[:]
             
+TIMESCALE = 40.0/1000.0 # ms
+SLICETIME = TIMESCALE / 10.0
+APPROXTIME = SLICETIME/8.0
 
+def priorities(c, board):
+    (col,val) = c
+    if board[col][1] == val-1:
+        return val - 1
+    if board[col][1] >= val:
+        return 5
+    if val == 5:
+        return 15
+    return 6 + (4 - val)
+    
+
+
+SENT = 0
+ERRORS = 0
+COUNT = 0
+
+CAREFUL = True
+        
+class TimedPlayer(object):
+    def __init__(self, name, pnr):
+        self.name = name
+        self.explanation = []
+        self.last_tick = time.time()
+        self.pnr = pnr
+        self.last_played = False
+        self.tt = time.time()
+    def get_action(self, nr, hands, knowledge, trash, played, board, valid_actions, hints):
+        global SENT, ERRORS, COUNT
+        tick = time.time()
+        duration = round((tick - self.last_tick)/SLICETIME)
+        other = (self.pnr + 1)% len(hands)
+        #print(self.pnr, "got", duration)
+        if duration >= 10:
+            duration = 9
+        if duration != SENT:
+            ERRORS += 1
+            #print("mismatch", nr, f(hands), f(board), duration, SENT)
+        COUNT += 1
+        other_hand = hands[other][:]
+        def prio(c):
+            return priorities(c,board)
+        other_hand.sort(key=prio)
+        #print(f(other_hand), f(board), list(map(prio, other_hand)), f(hands))
+        p = prio(other_hand[0])
+        delta = 0.0
+        if p >= 5:
+            delta += 5
+        #print("idx", hands[other].index(other_hand[0]))
+        def fix(n):
+            if n >= len(other_hand):
+               return len(other_hand) - 1
+            return int(round(n))
+        delta += hands[other].index(other_hand[0])
+        if duration >= 5:
+            action = Action(DISCARD, cnr=fix(duration-5))
+        else:
+            action = Action(PLAY, cnr=fix(duration))
+        if self.last_played and hints > 0 and CAREFUL:
+            action = Action(HINT_COLOR, pnr=other, col=other_hand[0][0])
+        t1 = time.time()
+        SENT = delta
+        #print(self.pnr, "convey", round(delta))
+        delta -= 0.5
+        while (t1 - tick) < delta*SLICETIME:
+            time.sleep(APPROXTIME)
+            t1 = time.time()
+        self.last_tick = time.time()
+        return action
+    def inform(self, action, player, game):
+        self.last_played = (action.type == PLAY)
+        self.last_tick = self.tt
+        self.tt = time.time()
+        #print(action, player)
+    def get_explanation(self):
+        return self.explanation
             
             
 CANDISCARD = 128
@@ -1353,7 +1431,7 @@ class NullStream(object):
         
 random.seed(123)
 
-playertypes = {"random": Player, "inner": InnerStatePlayer, "outer": OuterStatePlayer, "self": SelfRecognitionPlayer, "intentional": IntentionalPlayer, "sample": SamplingRecognitionPlayer, "full": SelfIntentionalPlayer}
+playertypes = {"random": Player, "inner": InnerStatePlayer, "outer": OuterStatePlayer, "self": SelfRecognitionPlayer, "intentional": IntentionalPlayer, "sample": SamplingRecognitionPlayer, "full": SelfIntentionalPlayer, "timed": TimedPlayer}
 names = ["Shangdi", "Yu Di", "Tian", "Nu Wa", "Pangu"]
         
         
